40e6a20a390f3b6d5816a43d777a67a2cf4923cd
Kui Shanya <rxs2833@g.rit.edu>
2024-06-24 00:05:20 -0400

    added pagination

--- client/client.js
+++ client/client.js
@@ -1,14 +1,20 @@
 var app,
-    recs,
     animeCards = { props: ['rec', 'titleLanguage'], methods: {
         getImageUrl: function(image) {
             return 'background-image: url("' + image + '")';
+        },
+        description: function(e, id) {
+            e.preventDefault();
+            rec = app.recs.find( rec => {
+                return rec.id === id;
+            });
+            rec.displayDesc = !rec.displayDesc;
         }
     },
     template: `
         <div class="col">
             <div class="card h-100">
-                <button type="button" class="btn btn-info btn-description-data" v-bind:data-anime-id="rec.id">
+                <button type="button" class="btn btn-info btn-description-data" v-on:click="description($event, rec.id)">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
@@ -28,9 +34,9 @@ var app,
                     <p>{{rec.episodes}} Episodes</p>
                 </div>
                 <div class="card-footer">
-                    <div class="overflow-x-auto site-link-container">
-                        <div v-for="(link,index) in rec.externalLinks" v-bind:class="{'last-link': index === rec.externalLinks.length -1, 'card-iconlink': true }">
-                            <a :href="(link.url)">
+                    <div class="overflow-x-auto site-link-container" v-bind:class="{'empty-links': rec.externalLinks.length < 1}">
+                        <div v-for="(link,index) in rec.externalLinks" v-bind:class="[{'last-link': index === rec.externalLinks.length -1}, 'card-iconlink']">
+                            <a :href="(link.url)"  target="_blank">
                                 <img class="site-icon" :src="(link.icon)" v-bind:style="{backgroundColor: link.color}"> </img>
                             </a>
                         </div>
@@ -52,6 +58,8 @@ $(document).ready(function(){
         },
         data: {
             recs: [],
+            fullRec: [],
+            currentPage: 1,
             titleLanguage: "english",
             loading: false,
             sources: [],
@@ -92,12 +100,43 @@ $(document).ready(function(){
         app.sources = rec.sources;
     });
     $('input[type="range"], input[type="checkbox"]').on('change', function(){
-        app.recs.map((rec)=> {
+        app.fullRec.map((rec)=> {
             var format = app.formats.find((format) => format.value === rec.format);
             rec.show = format.active && rec.seasonYear >= app.startYear && rec.seasonYear <= app.endYear;
             return rec;
         });
+        refreshPage();
     });
+    $('.page-link').on('click', function(e) {
+        e.preventDefault();
+        app.currentPage = e.currentTarget.dataset.page;
+        refreshPage();
+    })
+
+    function refreshPage() {
+        var shownRecs = app.fullRec.filter((rec) => rec.show),
+            pages = Math.ceil(shownRecs.length / 25);
+        if (app.currentPage > pages && pages > 0) {
+            app.currentPage = pages;
+        }
+        app.recs = shownRecs.slice((app.currentPage - 1) * 25 , app.currentPage * 25);
+        for(i = 1; i <= 8; i++) {
+            var pageButton = document.getElementById(i + '-page-link');
+            if (i > pages) {
+                pageButton.classList.add('disabled')
+            }
+            else {
+                pageButton.classList.remove('disabled');
+            }
+        }
+        $(document).ready(function(e) {
+            Array.from(document.getElementsByClassName("site-link-container")).forEach((siteCon) => {
+                if (siteCon.scrollWidth <= siteCon.clientWidth) {
+                    siteCon.classList.add("non-scroll-container");
+                }
+            })
+        });
+    }
 
     $("#searchForm").submit(function(e) {
         var action = $("#searchForm").attr("action");
@@ -120,7 +159,8 @@ $(document).ready(function(){
                     rec.show = true;
                     rec.displayDesc = false;
                 })
-                app.recs = result;
+                app.recs = result.slice(0, 25);
+                app.fullRec = result;
                 app.loading = false
                 $(document).ready(function(e) {
                     Array.from(document.getElementsByClassName("site-link-container")).forEach((siteCon) => {
@@ -128,15 +168,6 @@ $(document).ready(function(){
                             siteCon.classList.add("non-scroll-container");
                         }
                     })
-                    
-                    $('.btn-description-data').on('click',function(e) {
-                        e.preventDefault();
-                        var button = $(e.currentTarget),
-                        rec = app.recs.find( rec => {
-                            return rec.id === button.data('animeId');
-                        });
-                        rec.displayDesc = !rec.displayDesc;
-                    });
                 });
             },
             error: function(error, status, xhr) {
